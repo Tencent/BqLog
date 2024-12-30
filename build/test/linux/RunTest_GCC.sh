3a0ff8b7abdbbdcd8190150fb56dc799ea39bad6
pippocao <pippocao@tencent.com>
2024-12-30 21:40:57 +0800

    revert build script

--- build/test/linux/RunTest_GCC.sh
+++ build/test/linux/RunTest_GCC.sh
@@ -2,20 +2,11 @@
 
 mkdir CMakeFiles;
 cd CMakeFiles;
-# Enable core dumps
-ulimit -c unlimited
-export CORE_PATTERN=core.dump
-CC=gcc CXX=g++ cmake -DTARGET_PLATFORM:STRING=linux -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="-fsanitize=address" ../../../../test;
+CC=gcc CXX=g++ cmake -DTARGET_PLATFORM:STRING=linux -DCMAKE_BUILD_TYPE=RelWithDebInfo ../../../../test;
 make;
 ./BqLogUnitTest
 exit_code=$?
 cd ..;
-# Check if core dump was generated
-if [ -f "core.dump" ]; then
-    echo "Core dump generated."
-else
-    echo "No core dump generated."
-fi
 if [ $exit_code -eq 0 ]; then
     echo "Test succeeded."
 else
