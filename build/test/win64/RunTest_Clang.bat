349974774e9bb13fc6c5f3b6bf7ab9562cb009e3
pippocao <pippocao@tencent.com>
2025-04-27 22:32:00 +0800

    try use MSBuild as default VS entry to avoid license expiration

--- build/test/win64/RunTest_Clang.bat
+++ build/test/win64/RunTest_Clang.bat
@@ -8,6 +8,28 @@ IF NOT EXIST %VS_PATH%\devenv.com (
 )
 set VS_PATH=%VS_PATH:~1,-1%
 
+set BUILD_TOOL=
+set MSBUILD_PATH=
+set DEVENV_PATH=%VS_PATH%\devenv.com
+
+set VSWHERE="C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist %VSWHERE% (
+    for /f "tokens=*" %%i in ('%VSWHERE% -latest -products * -requires Microsoft.Component.MSBuild -property installationPath') do set VS_INSTALL_PATH=%%i
+    if defined VS_INSTALL_PATH (
+        set MSBUILD_PATH=%VS_INSTALL_PATH%\MSBuild\Current\Bin\MSBuild.exe
+        if exist "!MSBUILD_PATH!" (
+            set BUILD_TOOL=MSBUILD
+        )
+    )
+)
+
+if not defined BUILD_TOOL (
+    set BUILD_TOOL=DEVENV
+)
+
+set ARCH_PARAM=x64
+if "%PROCESSOR_ARCHITECTURE%"=="ARM64" set ARCH_PARAM=ARM64
+
 set CPP_VER_PARAM=%1
 if "%CPP_VER_PARAM%"=="" set CPP_VER_PARAM=17
 
@@ -16,8 +38,13 @@ cd VSProj
 
 cmake ..\..\..\..\test -DTARGET_PLATFORM:STRING=win64 -DJAVA_SUPPORT=ON -DCPP_VER=%CPP_VER_PARAM% -T ClangCl
 
-echo "%VS_PATH%\devenv.com"
-call "%VS_PATH%\devenv.com" ./BqLogUnitTest.sln /Rebuild "Debug" /Project "./BqLogUnitTest.vcxproj" /Out Build.log
+if "%BUILD_TOOL%"=="MSBUILD" (
+    echo Using MSBuild: !MSBUILD_PATH!
+    call "!MSBUILD_PATH!" ./BqLogUnitTest.sln /t:Rebuild /p:Configuration=Debug /p:Platform=%ARCH_PARAM% /out Build.log
+) else (
+    echo Using devenv.com: %DEVENV_PATH%
+    call "%DEVENV_PATH%" ./BqLogUnitTest.sln /Rebuild "Debug" /Project "./BqLogUnitTest.vcxproj" /Out Build.log
+)
 
 .\Debug\BqLogUnitTest.exe
 set exitcode=%ERRORLEVEL%
@@ -30,7 +57,11 @@ IF %exitcode% NEQ 0 (
     exit /b %exitcode%
 )
 
-call "%VS_PATH%\devenv.com" ./BqLogUnitTest.sln /Rebuild "RelWithDebInfo" /Project "./BqLogUnitTest.vcxproj" /Out Build.log
+if "%BUILD_TOOL%"=="MSBUILD" (
+    call "!MSBUILD_PATH!" ./BqLogUnitTest.sln /t:Rebuild /p:Configuration=RelWithDebInfo /p:Platform=%ARCH_PARAM% /out Build.log
+) else (
+    call "%DEVENV_PATH%" ./BqLogUnitTest.sln /Rebuild "RelWithDebInfo" /Project "./BqLogUnitTest.vcxproj" /Out Build.log
+)
 
 .\RelWithDebInfo\BqLogUnitTest.exe
 set exitcode=%ERRORLEVEL%
