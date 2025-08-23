10de3b0c4e3acb886cbaaf6fc7789fbdc2575805
pippocao <whataaa@foxmail.com>
2025-08-23 23:15:04 +0800

    Starvation-freedom

--- src/bq_common/platform/thread/spin_lock.h
+++ src/bq_common/platform/thread/spin_lock.h
@@ -197,10 +197,35 @@ namespace bq {
         class spin_lock_rw_crazy {
         private:
             static constexpr uint32_t timeout_ms = 60000U;
-            typedef bq::condition_type_t<sizeof(void*) == 4, int32_t, int64_t> counter_type;
-            static constexpr counter_type write_lock_mark_value = bq::condition_value < sizeof(void*) == 4, counter_type, (counter_type)INT32_MIN, (counter_type)INT64_MIN > ::value;
-            bq::cache_friendly_type<bq::platform::atomic<counter_type>> counter_;
-            bq::cache_friendly_type<bq::platform::atomic<counter_type>> writers_wait_counter_;
+            static constexpr int32_t write_lock_mark_value = INT32_MIN;
+            struct st_meta {
+            private:
+                uint64_t misc_;
+                bq::platform::atomic<int32_t> writers_wait_counter_;
+            public:
+                st_meta() : misc_(0), writers_wait_counter_(0){}
+                bq::platform::atomic<int32_t>& get_reader_counter()
+                {
+                    return *static_cast<bq::platform::atomic<int32_t>*>(static_cast<void*>(&misc_));;
+                }
+                bq::platform::atomic<uint64_t>& get_writer_counter()
+                {
+                    return *static_cast<bq::platform::atomic<uint64_t>*>(static_cast<void*>(&misc_));
+                }
+                bq::platform::atomic<int32_t>& get_writers_wait_counter()
+                {
+                    return writers_wait_counter_;
+                }
+                bq_forceinline static uint64_t generate_write_counter_value(int32_t reader_counter, uint32_t ticket)
+                {
+                    uint64_t result;
+                    *static_cast<bq::platform::atomic<int32_t>*>(static_cast<void*>(&result)) = reader_counter;
+                    *(static_cast<bq::platform::atomic<uint32_t>*>(static_cast<void*>(&result)) + 1) = ticket;
+                    return result;
+                }
+            };
+            bq::cache_friendly_type<bq::aligned_type<st_meta, sizeof(uint64_t)>> meta_;
+            bq::platform::atomic<uint32_t> ticket_seq_;
 #if !defined(NDEBUG)
             bq::platform::spin_lock lock_;
             struct debug_record {
@@ -218,11 +243,13 @@ namespace bq {
             void yield();
             void wait();
             uint64_t get_epoch();
+            bq_forceinline st_meta& get_meta()
+            {
+                return meta_.get().get();
+            }
 
         public:
-            spin_lock_rw_crazy()
-                : counter_(0)
-                , writers_wait_counter_(0)
+            spin_lock_rw_crazy(): meta_(st_meta()), ticket_seq_(0)
             {
             }
 
@@ -234,9 +261,9 @@ namespace bq {
 #if !defined(NDEBUG)
             void debug_output(int32_t pos)
             {
-                printf("record pos:%d, %" PRId64 ":[\n", pos, (int64_t)counter_.get().load_seq_cst());
+                printf("record pos:%d, %" PRIu32 ":[\n", pos, get_meta().get_writers_wait_counter().load_seq_cst());
                 for (auto item : record_) {
-                    printf("\t%" PRIu64 ", %s, %d\n", (uint64_t)item.tid_, item.is_write_ ? "true" : "false", item.phase_);
+                    printf("\t%" PRIu64 ", %s, %d\n", static_cast<uint64_t>(item.tid_), item.is_write_ ? "true" : "false", item.phase_);
                 }
                 printf("]n");
                 fflush(stdout);
@@ -253,18 +280,18 @@ namespace bq {
                 assert(reentrant_iter == record_.end() && "spin_lock_rw_crazy is not reentrant");
                 record_.push_back(debug_record { id, false, 0 });
                 lock_.unlock();
-                assert(writers_wait_counter_.get().load_relaxed() >= 0 && "invalid writers_wait_counter_.get()");
+                assert(get_meta().get_writers_wait_counter().load_relaxed() >= 0 && "invalid writers_wait_counter_.get()");
 #endif
                 while (true) {
-                    while (writers_wait_counter_.get().load_relaxed() > 0) {
-                        yield();
+                    while (get_meta().get_writers_wait_counter().load_relaxed() > 0) {
+                        wait();
                     }
-                    counter_type previous_counter = counter_.get().fetch_add_acq_rel(1);
+                    int32_t previous_counter = get_meta().get_reader_counter().fetch_add_acq_rel(1);
                     if (previous_counter >= 0) {
                         // read lock success.
                         break;
                     }
-                    counter_.get().fetch_sub_relaxed(1);
+                    get_meta().get_reader_counter().fetch_sub_relaxed(1);
 #if !defined(NDEBUG)
                     if (get_epoch() > start_epoch + timeout_ms) {
                         debug_output(1);
@@ -273,7 +300,7 @@ namespace bq {
 #endif
                     while (true) {
                         yield();
-                        counter_type current_counter = counter_.get().load_acquire();
+                        int32_t current_counter = get_meta().get_reader_counter().load_acquire();
                         if (current_counter >= 0) {
                             break;
                         }
@@ -303,12 +330,12 @@ namespace bq {
                 assert(reentrant_iter == record_.end() && "spin_lock_rw_crazy is not reentrant");
                 record_.push_back(debug_record { id, false, 0 });
                 lock_.unlock();
-                assert(writers_wait_counter_.get().load_relaxed() >= 0 && "invalid writers_wait_counter_.get()");
+                assert(get_meta().get_writers_wait_counter().load_relaxed() >= 0 && "invalid writers_wait_counter_.get()");
 #endif
-                while (writers_wait_counter_.get().load_relaxed() > 0) {
-                    yield();
+                while (get_meta().get_writers_wait_counter().load_relaxed() > 0) {
+                    wait();
                 }
-                counter_type previous_counter = counter_.get().fetch_add_acq_rel(1);
+                int32_t previous_counter = get_meta().get_reader_counter().fetch_add_acq_rel(1);
                 if (previous_counter >= 0) {
                     // read lock success.
 #if !defined(NDEBUG)
@@ -320,7 +347,7 @@ namespace bq {
 #endif
                     return true;
                 }
-                counter_.get().fetch_sub_relaxed(1);
+                get_meta().get_reader_counter().fetch_sub_relaxed(1);
 #if !defined(NDEBUG)
                 lock_.lock();
                 auto iter = record_.find(debug_record { id, false, 0 });
@@ -342,7 +369,7 @@ namespace bq {
                 lock_.unlock();
 #endif
 
-                counter_type previous_counter = counter_.get().fetch_sub_release(1);
+                int32_t previous_counter = get_meta().get_reader_counter().fetch_sub_release(1);
 #if !defined(NDEBUG)
                 assert(previous_counter > 0 && "spin_lock_rw_crazy counter error");
                 lock_.lock();
@@ -366,10 +393,15 @@ namespace bq {
                 lock_.unlock();
                 uint64_t start_epoch = get_epoch();
 #endif
-                writers_wait_counter_.get().fetch_add_relaxed(1);
+                get_meta().get_writers_wait_counter().fetch_add_relaxed(1);
+                auto my_ticket = ticket_seq_.fetch_add_relaxed(1);
                 while (true) {
-                    counter_type expected_counter = 0;
-                    if (counter_.get().compare_exchange_strong(expected_counter, write_lock_mark_value, bq::platform::memory_order::acq_rel, bq::platform::memory_order::acquire)) {
+                    uint64_t expected_counter =  st_meta::generate_write_counter_value(0, my_ticket);
+                    uint64_t write_lock_target_value = st_meta::generate_write_counter_value(write_lock_mark_value, my_ticket + 1);
+                    if (get_meta().get_writer_counter().compare_exchange_strong(expected_counter
+                                            , write_lock_target_value
+                                            , bq::platform::memory_order::acq_rel
+                                            , bq::platform::memory_order::acquire)) {
                         break;
                     }
                     yield();
@@ -399,9 +431,13 @@ namespace bq {
                 record_.push_back(debug_record { id, true, 0 });
                 lock_.unlock();
 #endif
-                writers_wait_counter_.get().fetch_add_relaxed(1);
-                counter_type expected_counter = 0;
-                if (counter_.get().compare_exchange_strong(expected_counter, write_lock_mark_value, bq::platform::memory_order::acq_rel, bq::platform::memory_order::acquire)) {
+                auto my_ticket = ticket_seq_.fetch_add_relaxed(1);
+                uint64_t expected_counter =  st_meta::generate_write_counter_value(0, my_ticket);
+                uint64_t write_lock_target_value = st_meta::generate_write_counter_value(write_lock_mark_value, my_ticket + 1);
+                if (get_meta().get_writer_counter().compare_exchange_strong(expected_counter
+                                                , write_lock_target_value
+                                                , bq::platform::memory_order::acq_rel
+                                                , bq::platform::memory_order::acquire)) {
 #if !defined(NDEBUG)
                     lock_.lock();
                     auto iter = record_.find({ id, true, 0 });
@@ -418,7 +454,6 @@ namespace bq {
                 record_.erase(iter);
                 lock_.unlock();
 #endif
-                writers_wait_counter_.get().fetch_add_relaxed(-1);
                 return false;
             }
 
@@ -435,8 +470,12 @@ namespace bq {
 #endif
 
                 while (true) {
-                    counter_type expected_counter = write_lock_mark_value;
-                    if (counter_.get().compare_exchange_strong(expected_counter, 0, bq::platform::memory_order::acq_rel, bq::platform::memory_order::acquire)) {
+                    int32_t expected_counter = write_lock_mark_value;
+                    if (get_meta().get_reader_counter().compare_exchange_strong(
+                                                expected_counter
+                                                , 0
+                                                , bq::platform::memory_order::acq_rel
+                                                , bq::platform::memory_order::acquire)) {
                         break;
                     }
 #if !defined(NDEBUG)
@@ -454,7 +493,7 @@ namespace bq {
                 record_.erase(iter);
                 lock_.unlock();
 #endif
-                writers_wait_counter_.get().fetch_add_relaxed(-1);
+                get_meta().get_writers_wait_counter().fetch_sub_relaxed(1);
             }
         };
 
