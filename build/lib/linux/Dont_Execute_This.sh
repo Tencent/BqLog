18d811e0cb515074712f067f0884aac7eebdbffa
pippocao <pippocao@tencent.com>
2025-09-09 22:49:51 +0800

    Build Scripts Update

--- build/lib/linux/Dont_Execute_This.sh
+++ build/lib/linux/Dont_Execute_This.sh
@@ -9,14 +9,14 @@ CONFIG_TYPE=(Debug MinSizeRel RelWithDebInfo Release)
 for i in "${!CONFIG_TYPE[@]}"; do
     config_type="${CONFIG_TYPE[$i]}"
     rm -rf $BUILD_TYPE/$config_type
-    mkdir $BUILD_TYPE/$config_type
+    mkdir -p $BUILD_TYPE/$config_type
     pushd "$BUILD_TYPE/$config_type" >/dev/null
     CC=$COMPILER_TYPE_C CXX=$COMPILER_TYPE_CXX cmake ../../../../../src \
     -DJAVA_SUPPORT=ON \
     -DTARGET_PLATFORM:STRING=linux \
 	  -DCMAKE_BUILD_TYPE=$config_type \
 	  -DBUILD_TYPE=$BUILD_TYPE
-      
+
 	cmake --build . --parallel
     cmake --build . --target install
     popd >/dev/null
