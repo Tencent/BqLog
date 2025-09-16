90cf107da4a4ef2793f75e52f3529b29ebc14e7f
eggdai <eggdai@tencent.com>
2025-09-16 17:08:24 +0800

    build tools for harmony platform by win

--- build/lib/harmony/Win_BuildAll_AndPack.bat
+++ build/lib/harmony/Win_BuildAll_AndPack.bat
@@ -14,6 +14,23 @@ Echo Android NDK Found:%OHOS_NDK_ROOT%
 
 echo Android NDK found: %OHOS_NDK_ROOT%
 
+echo Android NDK found: %ANDROID_NDK_ROOT%
+
+REM Try to use windows-arm64 toolchain if it exists, otherwise fallback to windows-x86_64
+set "HOST_TAG=windows-x86_64"
+if exist "%ANDROID_NDK_ROOT%\toolchains\llvm\prebuilt\windows-arm64\bin" (
+    set "HOST_TAG=windows-arm64"
+    echo Using NDK host toolchain: windows-arm64
+) else (
+    if exist "%ANDROID_NDK_ROOT%\toolchains\llvm\prebuilt\windows-x86_64\bin" (
+        set "HOST_TAG=windows-x86_64"
+        echo Using NDK host toolchain: windows-x86_64
+    ) else (
+        echo ERROR: Neither windows-arm64 nor windows-x86_64 toolchain found in your NDK. Build cancelled!
+        goto :EOF
+    )
+)
+
 set BUILD_TARGET[0]=arm64-v8a
 set BUILD_TARGET[1]=x86_64
 set BUILD_TYPE[0]=Debug
@@ -44,8 +61,8 @@ for /l %%a in (0,1) do (
 
 				 rem "%OHOS_NDK_ROOT%/native/build-tools/cmake/bin/cmake.exe" --trace -j10
 				 "%OHOS_NDK_ROOT%/native/build-tools/cmake/bin/ninja.exe" -f build.ninja
-				 rem move /Y ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog.so ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog_Symbol.so
-				 rem %ANDROID_NDK_ROOT%\toolchains\llvm\prebuilt\%HOST_TAG%\bin\llvm-strip.exe -s ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog_Symbol.so -o ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog.so
+				 move /Y ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog.so ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog_Symbol.so
+				 %ANDROID_NDK_ROOT%\toolchains\llvm\prebuilt\%HOST_TAG%\bin\llvm-strip.exe -s ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog_Symbol.so -o ..\..\..\..\..\install\dynamic_lib\lib\%%j\%%p\libBqLog.so
 			)
 			popd
 		)
@@ -58,14 +75,14 @@ rem call gradlew.bat assemble
 rem if errorlevel 1 goto :fail
 rem popd
 rem 
-rem if exist "pack" rd /s /q "pack"
-rem md pack
-rem pushd pack
-rem 
-rem cmake ..\..\..\..\pack -DTARGET_PLATFORM:STRING=harmony -DPACKAGE_NAME:STRING=bqlog-lib
-rem cmake --build . --target package
-rem if errorlevel 1 goto :fail
-rem popd
+if exist "pack" rd /s /q "pack"
+md pack
+pushd pack
+
+"%OHOS_NDK_ROOT%/native/build-tools/cmake/bin/cmake.exe" ..\..\..\..\pack -DTARGET_PLATFORM:STRING=harmony -DPACKAGE_NAME:STRING=bqlog-lib
+"%OHOS_NDK_ROOT%/native/build-tools/cmake/bin/cmake.exe" --build . --target package
+if errorlevel 1 goto :fail
+popd
 
  
 echo ---------
