1d4c76a107bf29f76daf24bb22c13a07cc089d89
eggdai <eggdai@tencent.com>
2025-09-18 15:39:22 +0800

    demo for harmony project

---
+++ build/demo/harmony/osProject/entry/src/main/cpp/napi_init.cpp
@@ -0,0 +1,61 @@
+#include "napi/native_api.h"
+#include "bq_log/bq_log.h"
+
+static napi_value Add(napi_env env, napi_callback_info info)
+{
+    size_t argc = 2;
+    napi_value args[2] = {nullptr};
+
+    napi_get_cb_info(env, info, &argc, args , nullptr, nullptr);
+
+    napi_valuetype valuetype0;
+    napi_typeof(env, args[0], &valuetype0);
+
+    napi_valuetype valuetype1;
+    napi_typeof(env, args[1], &valuetype1);
+
+    double value0;
+    napi_get_value_double(env, args[0], &value0);
+
+    double value1;
+    napi_get_value_double(env, args[1], &value1);
+
+    napi_value sum;
+    napi_create_double(env, value0 + value1, &sum);
+
+    return sum;
+
+}
+
+static napi_value NAPI_Global_api_get_log_version(napi_env env, napi_callback_info info)
+{
+    napi_value ver;
+    napi_create_string_utf8(env,bq::api::__api_get_log_version(), strlen(bq::api::__api_get_log_version()), &ver);
+    return ver;
+}
+
+EXTERN_C_START
+static napi_value Init(napi_env env, napi_value exports) {
+    napi_property_descriptor desc[] = {
+        {"add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"api_get_log_version", nullptr, NAPI_Global_api_get_log_version, nullptr, nullptr, nullptr, napi_default, nullptr }
+    };
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+    return exports;
+}
+EXTERN_C_END
+
+static napi_module demoModule = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = Init,
+    .nm_modname = "entry",
+    .nm_priv = ((void*)0),
+    .reserved = { 0 },
+};
+
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
+{
+    //napi_module_register(&demoModule);    
+}
---
+++ build/demo/harmony/osProject/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -0,0 +1,2 @@
+export const add: (a: number, b: number) => number;
+export const api_get_log_version:() => string;
