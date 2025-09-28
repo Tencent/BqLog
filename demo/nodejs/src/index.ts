6d4013db891f2d4f491a128bd36593da885847ff
pippocao <pippocao@tencent.com>
2025-09-28 22:56:31 +0800

    git ignore

--- demo/nodejs/src/index.ts
+++ demo/nodejs/src/index.ts
@@ -10,7 +10,7 @@ console.log(bq.log.get_version());
 let config = `appenders_config.appender_0.type=console
 appenders_config.appender_0.time_zone =default local time
 appenders_config.appender_0.levels=[verbose, debug, info, warning, error, fatal]
-appenders_config.appender_0.file_name=CCLog / normal
+appenders_config.appender_0.file_name=output
 appenders_config.appender_0.is_in_sandbox=false
 appenders_config.appender_0.max_file_size=10000000
 appenders_config.appender_0.expire_time_days=10
@@ -19,7 +19,7 @@ appenders_config.appender_0.capacity_limit=200000000
 appenders_config.appender_1.type=text_file
 appenders_config.appender_1.time_zone =default local time
 appenders_config.appender_1.levels=[verbose, debug, info, warning, error, fatal]
-appenders_config.appender_1.file_name=CCLog / normal
+appenders_config.appender_1.file_name=output
 appenders_config.appender_1.is_in_sandbox=false
 appenders_config.appender_1.max_file_size=1000000000
 appenders_config.appender_1.expire_time_days=10
@@ -29,7 +29,7 @@ appenders_config.appender_1.always_create_new_file=true
 appenders_config.appender_3409.type=compressed_file
 appenders_config.appender_3409.time_zone =default local time
 appenders_config.appender_3409.levels=[verbose, debug, info, warning, error, fatal]
-appenders_config.appender_3409.file_name=CCLog / normal
+appenders_config.appender_3409.file_name=output
 appenders_config.appender_3409.is_in_sandbox=false
 appenders_config.appender_3409.max_file_size=1000000000
 appenders_config.appender_3409.expire_time_days=10
