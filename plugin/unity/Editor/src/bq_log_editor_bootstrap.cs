94a2a8b7a2d9bd20846d5a7ccabb07feb35f86d9
pippocao <pippocao@tencent.com>
2025-10-10 17:19:13 +0800

    Compatible With Unity version older than Unity 2017

--- plugin/unity/Editor/src/bq_log_editor_bootstrap.cs
+++ plugin/unity/Editor/src/bq_log_editor_bootstrap.cs
@@ -13,53 +13,113 @@
 //  Redirect log console output to Unity Editor Console in Editor Mode
 //  Created by pippocao on 2025/10/10.
 
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
-using bq;
-using static Codice.CM.WorkspaceServer.WorkspaceTreeDataStore;
 
 namespace BqLog.Editor
 {
-#if UNITY_2017_1_OR_NEWER
     [InitializeOnLoad]
     public static class bq_log_editor_bootstrap
     {
+#if !UNITY_2017_1_OR_NEWER && !UNITY_5_3_OR_NEWER
+        private static bool was_compiling_;
+#endif
+
         static bq_log_editor_bootstrap()
         {
-            AssemblyReloadEvents.afterAssemblyReload -= OnAfterAssemblyReload;
-            AssemblyReloadEvents.afterAssemblyReload += OnAfterAssemblyReload;
+            // Try to register once on next editor update (after domain reload)
+            EditorApplication.delayCall += register_callback;
+
+#if UNITY_2017_1_OR_NEWER
+            AssemblyReloadEvents.afterAssemblyReload -= on_after_assembly_reload;
+            AssemblyReloadEvents.afterAssemblyReload += on_after_assembly_reload;
+#elif UNITY_5_3_OR_NEWER
+            // 5.3+：Use DidReloadScripts
+            // delayCall has already been used to register once after domain reload
+#else
+            // 5.0–5.2：no hook for domain reload, poll compilation state
+            was_compiling_ = EditorApplication.isCompiling;
+            EditorApplication.update -= poll_compilation_and_register;
+            EditorApplication.update += poll_compilation_and_register;
+#endif
         }
 
-        private static void bq_log_console_callback(ulong log_id, int category_idx, bq.def.log_level log_level, string content)
+#if UNITY_2017_1_OR_NEWER
+        private static void on_after_assembly_reload()
         {
-            switch (log_level)
+            register_callback();
+        }
+#elif UNITY_5_3_OR_NEWER
+        [UnityEditor.Callbacks.DidReloadScripts]
+        private static void on_after_assembly_reload()
+        {
+            register_callback();
+        }
+#else
+        private static void poll_compilation_and_register()
+        {
+            bool now = EditorApplication.isCompiling;
+            if (was_compiling_ && !now)
+            {
+                register_callback();
+            }
+            was_compiling_ = now;
+        }
+#endif
+
+        private static void register_callback()
+        {
+            Debug.Log("[BqLog] Try To Register Console Callback By Assembly Reload");
+            bq.log.register_console_callback(new bq.log.type_console_callback(console_callback));
+        }
+
+        private static void console_callback(ulong log_id, int category_idx, bq.def.log_level level, string content)
+        {
+            switch (level)
             {
                 case bq.def.log_level.verbose:
-                    Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, content);
-                    break;
                 case bq.def.log_level.debug:
-                    Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, content);
-                    break;
                 case bq.def.log_level.info:
-                    Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, content);
+                    log_no_stack(LogType.Log, content);
                     break;
                 case bq.def.log_level.warning:
-                    Debug.LogFormat(LogType.Warning, LogOption.NoStacktrace, null, content);
+                    log_no_stack(LogType.Warning, content);
                     break;
                 case bq.def.log_level.error:
-                    Debug.LogFormat(LogType.Error, LogOption.NoStacktrace, null, content);
+                    log_no_stack(LogType.Error, content);
                     break;
                 case bq.def.log_level.fatal:
-                    Debug.LogFormat(LogType.Exception, LogOption.NoStacktrace, null, content);
+                    log_no_stack(LogType.Exception, content);
+                    break;
+                default:
+                    log_no_stack(LogType.Log, content);
                     break;
             }
         }
 
-        private static void OnAfterAssemblyReload()
+        private static void log_no_stack(LogType type, string msg)
         {
-            Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, "[BqLog]Register console callback by Assembly Reload");
-            bq.log.register_console_callback(new log.type_console_callback(bq_log_console_callback));
+#if UNITY_2017_1_OR_NEWER
+            Debug.LogFormat(type, LogOption.NoStacktrace, null, msg);
+#else
+            switch (type)
+            {
+                case LogType.Warning:   
+                    Debug.LogWarning(msg); 
+                    break;
+                case LogType.Error:     
+                    Debug.LogError(msg);   
+                    break;
+                case LogType.Exception: 
+                    Debug.LogError(msg);   
+                    break;
+                default:                
+                    Debug.Log(msg);        
+                    break;
+            }
+#endif
         }
     }
-#endif
 }
+#endif
