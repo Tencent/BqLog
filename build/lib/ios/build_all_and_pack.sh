6a48e5f7acbd7f1221884d8b127119a4cc5cc0ea
pippocao <pippocao@tencent.com>
2025-10-10 15:36:09 +0800

    iOS should build .a and .dylib

--- build/lib/ios/build_all_and_pack.sh
+++ build/lib/ios/build_all_and_pack.sh
@@ -23,11 +23,50 @@ do
         exit 1
     fi
     cmake --install . --config $build_config
+done
 
-    if [ -d "../../../../artifacts/dynamic_lib/ios/${build_config}/BqLog.framework.dSYM" ]; then
-        rm -rf "../../../../dist/dynamic_lib/ios/${build_config}/BqLog.framework.dSYM"
-        cp -r "../../../../artifacts/dynamic_lib/ios/${build_config}/BqLog.framework.dSYM" "../../../../dist/dynamic_lib/ios/${build_config}/BqLog.framework.dSYM"
+cmake ../../../../src \
+    -G Xcode \
+    -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake \
+    -DPLATFORM=OS64 \
+    -DDEPLOYMENT_TARGET=9.0 \
+    -DBUILD_LIB_TYPE=static_lib \
+    -DAPPLE_LIB_FORMAT=a \
+    -DTARGET_PLATFORM:STRING=ios
+
+BUILD_CONFIGS=(Debug MinSizeRel Release RelWithDebInfo)
+for build_config in ${BUILD_CONFIGS[@]}
+do
+    cmake --build . --config $build_config
+    if [ $? -eq 0 ]; then
+        echo "Build succeeded."
+    else
+        echo "Build failed."
+        exit 1
     fi
+    cmake --install . --config $build_config
+done
+
+cmake ../../../../src \
+    -G Xcode \
+    -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake \
+    -DPLATFORM=OS64 \
+    -DDEPLOYMENT_TARGET=9.0 \
+    -DBUILD_LIB_TYPE=dynamic_lib \
+    -DAPPLE_LIB_FORMAT=dylib \
+    -DTARGET_PLATFORM:STRING=ios
+
+BUILD_CONFIGS=(Debug MinSizeRel Release RelWithDebInfo)
+for build_config in ${BUILD_CONFIGS[@]}
+do
+    cmake --build . --config $build_config
+    if [ $? -eq 0 ]; then
+        echo "Build succeeded."
+    else
+        echo "Build failed."
+        exit 1
+    fi
+    cmake --install . --config $build_config
 done
 
 popd >/dev/null
