372e3b6c8ec2a09ad446573dac3de3c2267a6d35
pippocao <pippocao@tencent.com>
2025-10-10 15:38:30 +0800

    iOS should build .a and .dylib and static framework

--- build/lib/ios/build_all_and_pack.sh
+++ build/lib/ios/build_all_and_pack.sh
@@ -30,8 +30,8 @@ cmake ../../../../src \
     -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake \
     -DPLATFORM=OS64 \
     -DDEPLOYMENT_TARGET=9.0 \
-    -DBUILD_LIB_TYPE=static_lib \
-    -DAPPLE_LIB_FORMAT=a \
+    -DBUILD_LIB_TYPE=dynamic_lib \
+    -DAPPLE_LIB_FORMAT=dylib \
     -DTARGET_PLATFORM:STRING=ios
 
 BUILD_CONFIGS=(Debug MinSizeRel Release RelWithDebInfo)
@@ -52,8 +52,30 @@ cmake ../../../../src \
     -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake \
     -DPLATFORM=OS64 \
     -DDEPLOYMENT_TARGET=9.0 \
-    -DBUILD_LIB_TYPE=dynamic_lib \
-    -DAPPLE_LIB_FORMAT=dylib \
+    -DBUILD_LIB_TYPE=static_lib \
+    -DAPPLE_LIB_FORMAT=framework \
+    -DTARGET_PLATFORM:STRING=ios
+
+BUILD_CONFIGS=(Debug MinSizeRel Release RelWithDebInfo)
+for build_config in ${BUILD_CONFIGS[@]}
+do
+    cmake --build . --config $build_config
+    if [ $? -eq 0 ]; then
+        echo "Build succeeded."
+    else
+        echo "Build failed."
+        exit 1
+    fi
+    cmake --install . --config $build_config
+done
+
+cmake ../../../../src \
+    -G Xcode \
+    -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake \
+    -DPLATFORM=OS64 \
+    -DDEPLOYMENT_TARGET=9.0 \
+    -DBUILD_LIB_TYPE=static_lib \
+    -DAPPLE_LIB_FORMAT=a \
     -DTARGET_PLATFORM:STRING=ios
 
 BUILD_CONFIGS=(Debug MinSizeRel Release RelWithDebInfo)
