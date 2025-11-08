1bcd55721909de4ff3112afecb47b89c9a23d37a
pippocao <whataaa@foxmail.com>
2025-11-08 13:12:38 +0800

    Fix test case errors

--- src/bq_log/types/buffer/group_list.cpp
+++ src/bq_log/types/buffer/group_list.cpp
@@ -311,7 +311,7 @@ namespace bq {
                 if (!src_node) {
                     src_node = bq::util::aligned_new<group_node>(BQ_CACHE_LINE_SIZE, this, max_block_count_per_group_, current_group_index_.add_fetch(1, bq::platform::memory_order::relaxed));
 #if defined(BQ_UNIT_TEST)
-                    if (src_node->get_memory_map_status() == create_memory_map_result::use_existed) {
+                    if (src_node && src_node->get_memory_map_status() == create_memory_map_result::use_existed) {
                         bq::util::log_device_console(bq::log_level::error, "group index:");
                         assert(false && "must use new memory map");
                     }
