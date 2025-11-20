4fce861a958265d4ca3de0dff178ab8613ab9ef8
pippocao <pippocao@tencent.com>
2025-11-20 16:02:02 +0800

    Fix errors

--- src/bq_log/types/buffer/group_list.cpp
+++ src/bq_log/types/buffer/group_list.cpp
@@ -101,7 +101,7 @@ namespace bq {
 
         create_memory_map_result result = create_memory_map_result::failed;
         size_t current_file_size = bq::file_manager::instance().get_file_size(memory_map_file_);
-        if (current_file_size == 0) {
+        if (bq::file_manager::get_and_clear_last_file_error() != 0) {
             bq::util::log_device_console(log_level::warning, "group node get file size of \"%s\" failed, error code:%" PRId32 ", use memory instead.", memory_map_file_.abs_file_path().c_str(), bq::file_manager::get_and_clear_last_file_error());
             bq::file_manager::instance().close_file(memory_map_file_);
             return create_memory_map_result::failed;
