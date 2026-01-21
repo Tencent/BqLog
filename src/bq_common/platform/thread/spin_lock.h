c9104f964a9d30b9e00cb2900828074e4037ca02
pippocao <pippocao@tencent.com>
2026-01-21 19:16:30 +0800

    Revert "Prevent starvation"
    This reverts commit 68ebab03e1c653c63c986b605290de3aaa239dc6.

--- src/bq_common/platform/thread/spin_lock.h
+++ src/bq_common/platform/thread/spin_lock.h
@@ -210,17 +210,15 @@ namespace bq {
 
         class spin_lock_rw_crazy {
         private:
-            bq::cache_friendly_type<bq::platform::atomic<uint64_t>> state_;
-
-            static constexpr uint64_t READER_MASK = 0xFFFFFFFFULL;
-            static constexpr uint64_t WRITER_WAITING_INC = 1ULL << 32;
-            static constexpr uint64_t WRITER_ACTIVE_BIT = 1ULL << 63;
-            // Any writer activity (waiting or active) - blocks new readers
-            static constexpr uint64_t WRITER_ANY_MASK = 0xFFFFFFFF00000000ULL; 
+            typedef bq::condition_type_t<sizeof(void*) == 4, int32_t, int64_t> counter_type;
+            static constexpr counter_type write_lock_mark_value = bq::condition_value<sizeof(void*) == 4, counter_type, (counter_type)INT32_MIN, (counter_type)INT64_MIN>::value;
+            bq::cache_friendly_type<bq::platform::atomic<counter_type>> counter_;
+            bq::platform::atomic<int32_t> pending_writers_;
 
         public:
             spin_lock_rw_crazy()
-                : state_(0)
+                : counter_(0)
+                , pending_writers_(0)
             {
             }
 
@@ -232,66 +230,76 @@ namespace bq {
             inline void read_lock()
             {
                 while (true) {
-                    uint64_t old = state_.get().load(bq::platform::memory_order::relaxed);
-                    if (old & WRITER_ANY_MASK) {
+                    if (pending_writers_.load(bq::platform::memory_order::acquire) > 0) {
                         bq::platform::thread::cpu_relax();
                         continue;
                     }
-                    if (state_.get().compare_exchange_weak(old, old + 1, bq::platform::memory_order::acquire, bq::platform::memory_order::relaxed)) {
+                    counter_type previous_counter = counter_.get().fetch_add_acq_rel(1);
+                    if (previous_counter >= 0) {
+                        // read lock success.
                         break;
                     }
-                    bq::platform::thread::cpu_relax();
+                    counter_.get().fetch_sub_relaxed(1);
+                    while (true) {
+                        bq::platform::thread::cpu_relax();
+                        counter_type current_counter = counter_.get().load_acquire();
+                        if (current_counter >= 0) {
+                            break;
+                        }
+                    }
                 }
             }
 
             inline bool try_read_lock()
             {
-                uint64_t old = state_.get().load(bq::platform::memory_order::relaxed);
-                if (old & WRITER_ANY_MASK) {
+                if (pending_writers_.load(bq::platform::memory_order::acquire) > 0) {
                     return false;
                 }
-                if (state_.get().compare_exchange_strong(old, old + 1, bq::platform::memory_order::acquire, bq::platform::memory_order::relaxed)) {
+                counter_type previous_counter = counter_.get().fetch_add_acq_rel(1);
+                if (previous_counter >= 0) {
+                    // read lock success.
                     return true;
                 }
+                counter_.get().fetch_sub_relaxed(1);
                 return false;
             }
 
             inline void read_unlock()
             {
-                state_.get().fetch_sub(1, bq::platform::memory_order::release);
+                counter_type previous_counter = counter_.get().fetch_sub_release(1);
+                (void)previous_counter;
             }
 
             inline void write_lock()
             {
-                state_.get().fetch_add(WRITER_WAITING_INC, bq::platform::memory_order::acq_rel);
-
+                pending_writers_.fetch_add(1, bq::platform::memory_order::acq_rel);
                 while (true) {
-                    uint64_t old = state_.get().load(bq::platform::memory_order::relaxed);
-                    
-                    if ((old & READER_MASK) == 0 && (old & WRITER_ACTIVE_BIT) == 0) {
-                        uint64_t desired = (old - WRITER_WAITING_INC) | WRITER_ACTIVE_BIT;
-                        if (state_.get().compare_exchange_weak(old, desired, bq::platform::memory_order::acquire, bq::platform::memory_order::relaxed)) {
-                            break;
-                        }
+                    counter_type expected_counter = 0;
+                    if (counter_.get().compare_exchange_strong(expected_counter, write_lock_mark_value, bq::platform::memory_order::acq_rel, bq::platform::memory_order::acquire)) {
+                        break;
                     }
                     bq::platform::thread::cpu_relax();
                 }
+                pending_writers_.fetch_sub(1, bq::platform::memory_order::release);
             }
 
             inline bool try_write_lock()
             {
-                uint64_t old = state_.get().load(bq::platform::memory_order::relaxed);
-                if (old == 0) { // optimization: check for clean state first
-                    if (state_.get().compare_exchange_strong(old, WRITER_ACTIVE_BIT, bq::platform::memory_order::acquire, bq::platform::memory_order::relaxed)) {
-                        return true;
-                    }
+                counter_type expected_counter = 0;
+                if (counter_.get().compare_exchange_strong(expected_counter, write_lock_mark_value, bq::platform::memory_order::acq_rel, bq::platform::memory_order::acquire)) {
+                    return true;
                 }
                 return false;
             }
 
             inline void write_unlock()
             {
-                state_.get().fetch_and(~WRITER_ACTIVE_BIT, bq::platform::memory_order::release);
+                while (true) {
+                    counter_type expected_counter = write_lock_mark_value;
+                    if (counter_.get().compare_exchange_strong(expected_counter, 0, bq::platform::memory_order::acq_rel, bq::platform::memory_order::acquire)) {
+                        break;
+                    }
+                }
             }
         };
 
